410502c33a13c73651d2e1370c8fd865fab783a6
apurbo <apurboroy7077@gmail.com>
2025-03-14 10:37:30 +0600

    update

--- src/helpers/checkIfUserWithSameEmailExists.ts
+++ src/helpers/checkIfUserWithSameEmailExists.ts
@@ -1,22 +1,20 @@
 import { userDataModelOfWeatherConsumerReport } from '../app/modules/user/userModelOfWeatherConsumerReport.model';
 
-export const checkIfUserWithSameEmailExists = (
+export const checkIfUserWithSameEmailExists = async (
   userEmail: string,
   userDataModel: typeof userDataModelOfWeatherConsumerReport
 ) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const users = await userDataModel.findOne({ email: userEmail });
-      if (!users) {
-        resolve(
-          'NO USER IS REGISTERED BY THIS EMAIL AND A EMAIL CAN BE CREATED WITH THIS EMAIL'
-        );
-      } else {
-        reject('This user is already taken');
-      }
-    } catch (error) {
-      console.log(error);
-      reject(error);
+  try {
+    const user = await userDataModel.findOne({ email: userEmail });
+    if (!user) {
+      return 'NO USER IS REGISTERED BY THIS EMAIL, AND AN EMAIL CAN BE CREATED WITH THIS EMAIL';
+    } else {
+      throw new Error('This user is already taken');
     }
-  });
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(
+      'An error occurred while checking user email: ' + error.message
+    );
+  }
 };
